4cd2a3d2bd08f4234d696d2fe03814b4eb0f04ad
herethesavior <rdelacruz@transurban.com>
2023-03-14 23:08:17 +0800

    sample: added unformatted code

---
+++ force-app/main/default/aura/continuationAura/continuationAuraController.js
@@ -0,0 +1,29 @@
+({
+  callContinuation: function (cmp) 
+  {
+var action = cmp.get("c.startRequest");
+action.setCallback(this, function (response) 
+{
+  var state = response.getState();
+  if (state === "SUCCESS") {
+    console.log(
+      "From server: " + response.getReturnValue() + "\n" + JSON.stringify(response.getReturnValue())
+    );
+      } else if (state === "INCOMPLETE") {
+    alert("Continuation action is INCOMPLETE");
+  } else if (state === "ERROR") {
+    var errors = response.getError();
+  if (errors) {
+      if (errors[0] && errors[0].message) 
+      {
+        console.log("Error message: " + errors[0].message);
+      }
+    } else {
+        console.log("Unknown error");
+      }
+      }
+    });
+    // Enqueue action that returns a continuation
+$A.enqueueAction(action);
+  },
+});
