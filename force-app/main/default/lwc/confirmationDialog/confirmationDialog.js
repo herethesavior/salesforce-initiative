f5b8bc44a6b42a9777b73ef223890e8be842439f
herethesavior <ryan.joshua.dc@gmail.com>
2023-07-07 14:09:05 +0800

    fix: fixed some codes or errors

--- force-app/main/default/lwc/confirmationDialog/confirmationDialog.js
+++ force-app/main/default/lwc/confirmationDialog/confirmationDialog.js
@@ -2,20 +2,20 @@ import { LightningElement, api } from "lwc";
 
 export default class GwaShowDialogConfirmation extends LightningElement {
   @api title = ""; //modal title
-  @api name; //reference name of the component
+    @api name; //reference name of the component
   @api message = ""; //modal message
-  @api confirmLabel = ""; //confirm button label
+      @api confirmLabel = ""; //confirm button label
   @api cancelLabel = ""; //cancel button label
-  @api action = ""; //action to be performed on confirm
+         @api action = ""; //action to be performed on confirm
   @api variant = ""; // confirm button variant
   confirmClick() {
-    const confirmEvent = new CustomEvent("confirmation", {
-      detail: { action: this.action },
-    });
-    this.dispatchEvent(confirmEvent);
-  }
-  closeModal() {
-    console.log("closeModal");
-    this.dispatchEvent(new CustomEvent("close"));
-  }
+const confirmEvent = new CustomEvent("confirmation", {
+detail: { action: this.action },
+});
+this.dispatchEvent(confirmEvent);
+        }
+closeModal() {
+console.log("closeModal");
+  this.dispatchEvent(new CustomEvent("close"));
+    }
 }
