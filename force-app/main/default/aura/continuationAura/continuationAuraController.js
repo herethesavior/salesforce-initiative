ed67e8c33311b2affa431a195817ade60fe77d3e
Ryan Joshua Dela Cruz <83619637+herethesavior@users.noreply.github.com>
2023-09-11 07:11:24 +0000

    chore: format source code with Prettier

--- force-app/main/default/aura/continuationAura/continuationAuraController.js
+++ force-app/main/default/aura/continuationAura/continuationAuraController.js
@@ -1,21 +1,21 @@
 ({
   callContinuation: function (cmp) {
-  var action = cmp.get("c.startRequest");
-  action.setCallback(this, function (response) {
-  var state = response.getState();
-if (state === "SUCCESS") {
+    var action = cmp.get("c.startRequest");
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
         console.log(
-      "From server: " +
+          "From server: " +
             response.getReturnValue() +
             "\n" +
             JSON.stringify(response.getReturnValue())
-    );
-  } else if (state === "INCOMPLETE") {
+        );
+      } else if (state === "INCOMPLETE") {
         alert("Continuation action is INCOMPLETE");
       } else if (state === "ERROR") {
         var errors = response.getError();
         if (errors) {
-    if (errors[0] && errors[0].message) {
+          if (errors[0] && errors[0].message) {
             console.log("Error message: " + errors[0].message);
           }
         } else {
